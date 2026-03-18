1a66b42fb698915c1e5d46b05f86a516e5f8faa0
artemmazheykin <morodin.bk@gmail.com>
2026-03-18 09:14:23 +0400

    Initial commit

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by  Artem Mazheykin on 18.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
